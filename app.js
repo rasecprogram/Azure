26b77c03835984f750148966a76850a0e7c7c3c2
rasecprogram <neji.hiuga2535@gmail.com>
2026-06-29 10:08:28 -0300

    Versão 2.0

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
+
+    const ambiente = process.env.ENV_NAME || "local";
+
     res.send(`
         <h1>Aplicação Node.js no Azure App Service</h1>
         <p>Deploy realizado via GitHub Actions!</p>
+
+        <h2 style="color: blue;">
+            🚀 VERSÃO 2.0 🚀
+        </h2>
+
+        <h3 style="background-color: yellow; display: inline-block; padding:8px;">
+            Ambiente Atual: ${ambiente}
+        </h3>
     `);
 });
 
